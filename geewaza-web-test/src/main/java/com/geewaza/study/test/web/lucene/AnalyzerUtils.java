fc6b8e0c4b17323a7aedbee4f1cfddb44dc9e30f
frank59 <380330439@qq.com>
2016-02-28 23:43:52 +0800

    lucene：自定义同义词分词器

--- geewaza-web-test/src/main/java/com/geewaza/study/test/web/lucene/AnalyzerUtils.java
+++ geewaza-web-test/src/main/java/com/geewaza/study/test/web/lucene/AnalyzerUtils.java
@@ -2,7 +2,7 @@ package com.geewaza.study.test.web.lucene;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.tokenattributes.TermAttribute;
+import org.apache.lucene.analysis.tokenattributes.*;
 
 import java.io.IOException;
 import java.io.StringReader;
@@ -22,5 +22,29 @@ public class AnalyzerUtils {
 			System.out.print("[" + term.term() + "]");
 		}
 		System.out.println();
+	}
+
+	public static void displayAllTokenInfo(String str,Analyzer a) {
+		try {
+			TokenStream stream = a.tokenStream("content",new StringReader(str));
+			//位置增量的属性，存储语汇单元之间的距离
+			PositionIncrementAttribute pia =
+					stream.addAttribute(PositionIncrementAttribute.class);
+			//每个语汇单元的位置偏移量
+			OffsetAttribute oa;
+			oa = stream.addAttribute(OffsetAttribute.class);
+			//存储每一个语汇单元的信息（分词单元信息）
+			CharTermAttribute cta =
+					stream.addAttribute(CharTermAttribute.class);
+			//使用的分词器的类型信息
+			TypeAttribute ta =
+					stream.addAttribute(TypeAttribute.class);
+			for(;stream.incrementToken();) {
+				System.out.print(pia.getPositionIncrement()+":");
+				System.out.print(cta+"["+oa.startOffset()+"-"+oa.endOffset()+"]-->"+ta.type()+"\n");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
