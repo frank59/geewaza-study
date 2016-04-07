d2986a046ed6d40e6ca5bb53c217e32cdf58df6d
wangheng <380330439@qq.com>
2016-04-07 20:53:05 +0800

    完成一个算法题

--- geewaza-web-test/src/main/java/com/geewaza/study/test/web/leetcode/BestTimeToBuyAndSellStock.java
+++ geewaza-web-test/src/main/java/com/geewaza/study/test/web/leetcode/BestTimeToBuyAndSellStock.java
@@ -2,19 +2,18 @@ package com.geewaza.study.test.web.leetcode;
 
 /**
  * Created by WangHeng on 2016/3/31.
+ * 122. Best Time to Buy and Sell Stock II
  */
 public class BestTimeToBuyAndSellStock {
 
 
     public int maxProfit(int[] prices) {
         int total = 0;
-        int last = 0;
-        int buy = 0;
         for(int i = 1; i < prices.length; i++) {
-
-
+            if (prices[i - 1] < prices[i]) {
+                total += prices[i] - prices[i - 1];
+            }
         }
-
         return total;
     }
 }
