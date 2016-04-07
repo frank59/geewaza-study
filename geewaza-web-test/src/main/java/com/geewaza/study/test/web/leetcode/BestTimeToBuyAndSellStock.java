c31caba803a83baad5e9557761695cea91aa009a
wangheng <380330439@qq.com>
2016-04-07 19:54:37 +0800

    增加调用solr进行分词的例子程序

---
+++ geewaza-web-test/src/main/java/com/geewaza/study/test/web/leetcode/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,20 @@
+package com.geewaza.study.test.web.leetcode;
+
+/**
+ * Created by WangHeng on 2016/3/31.
+ */
+public class BestTimeToBuyAndSellStock {
+
+
+    public int maxProfit(int[] prices) {
+        int total = 0;
+        int last = 0;
+        int buy = 0;
+        for(int i = 1; i < prices.length; i++) {
+
+
+        }
+
+        return total;
+    }
+}
