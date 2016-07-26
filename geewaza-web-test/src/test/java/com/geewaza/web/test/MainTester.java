59cfad62f897ec12bd21aed63a4fb7d8b6d00e89
wangheng <380330439@qq.com>
2016-07-26 11:21:49 +0800

    增加一个小工具

--- geewaza-web-test/src/test/java/com/geewaza/web/test/MainTester.java
+++ geewaza-web-test/src/test/java/com/geewaza/web/test/MainTester.java
@@ -1,5 +1,7 @@
 package com.geewaza.web.test;
 
+import com.geewaza.study.commons.Request;
+import org.apache.commons.io.FileUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.concurrent.FutureCallback;
@@ -11,15 +13,47 @@ import org.apache.http.nio.client.methods.HttpAsyncMethods;
 import org.apache.http.nio.protocol.HttpAsyncRequestProducer;
 import org.apache.http.protocol.HttpContext;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.CharBuffer;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.zip.GZIPOutputStream;
 
 public class MainTester {
-	public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
-		test01();
+	public static void main(String[] args) throws Exception {
+		test03();
+	}
+
+	private static void test03() throws Exception {
+		String inputFile = "/tmp/file/inputFile.dat";
+		String outPutFile1 = "/tmp/file/gzipOutPutFile1.dat";
+		String outPutFile2 = "/tmp/file/gzipOutPutFile2.dat";
+		String response = Request.requestGet("http://10.103.88.189/searches/soku/direct/v4/by_keywords.json?keyword=%E5%A4%9A%E5%95%A6a%E6%A2%A6&searchType=2");
+		OutputStream ops = new GZIPOutputStream(new FileOutputStream(outPutFile2));
+		ops.write(response.getBytes());
+		ops.close();
+		FileUtils.writeByteArrayToFile(new File(outPutFile1), response.getBytes());
+
+
+	}
+
+	private static void test02() throws ParseException {
+		String dateStr = "2012-05-07 07-58-28";
+		int second = 5000;
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(sdf.parse(dateStr));
+		calendar.add(Calendar.SECOND, second);
+		String targetDate = sdf.format(calendar.getTime());
+		System.out.println(dateStr + " ---> " + targetDate);
+		calendar.add(Calendar.SECOND, second * -1);
+		System.out.println(targetDate + " ---> " + sdf.format(calendar.getTime()));
+
 	}
 
 	private static void test01() throws ExecutionException, InterruptedException, IOException {
