fbe84cb3f9b82c926a6365e91f0f60df611916b7
wangheng <380330439@qq.com>
2016-08-19 19:15:52 +0800

    修改Jedis工具类以适应2.8.0版本的jedis client

--- geewaza-web-test/src/main/java/com/geewaza/study/test/web/redis/RedisCloudTester.java
+++ geewaza-web-test/src/main/java/com/geewaza/study/test/web/redis/RedisCloudTester.java
@@ -6,6 +6,8 @@ import com.geewaza.study.commons.jedis.ShardedRedisManager;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by WangHeng on 2016/8/19.
  */
@@ -13,7 +15,24 @@ public class RedisCloudTester {
 
 
     public static void main(String[] args) throws Exception {
-        test01();
+        test03();
+    }
+
+    private static void test03() throws Exception {
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring-config.xml");
+        SentinelRedisManager redisManager = context.getBean("sentinelRedisManager", SentinelRedisManager.class);
+        for (int i = 1; i < Integer.MAX_VALUE; i++) {
+            try {
+                redisManager.set("age", i + "");
+                System.out.println("set age<--" + i);
+                Integer getValue = Integer.parseInt(redisManager.get("age"));
+                System.out.println("get age-->" + getValue);
+                System.out.println(i + "=" + getValue);
+                TimeUnit.SECONDS.sleep(1);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private static void test02() throws Exception {
